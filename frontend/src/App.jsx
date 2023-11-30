866ac1ade6dbf32100bbf6fc7eab00986516aa2b
blackPeppper <spices.digital@gmail.com>
2023-11-30 18:05:13 +0200

    file uploader

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,46 @@
-import {Button} from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
+import { Icon } from "@iconify/react";
 
 function App() {
-
   return (
     <>
-      <Button>adwdwad</Button>
+      <div className="container mx-auto px-3">
+        <br />
+        <div className="flex gap-5">
+          <Icon
+            icon={"solar:zip-file-bold-duotone"}
+            className="text-5xl drop-shadow-md text-blue-500"
+          />
+          <Icon
+            icon={"solar:folder-bold-duotone"}
+            className="text-5xl drop-shadow-md text-blue-500"
+          />
+          <Icon
+            icon={"solar:file-bold-duotone"}
+            className="text-5xl drop-shadow-md text-blue-500"
+          />
+        </div>
+        <hr className="my-4" />
+        <div className="group">
+          <div className="border-3 group-hover:border-blue-500 group-hover:text-blue-500 transition-all relative p-5 h-52 rounded-lg border-dotted flex justify-center items-center">
+            <div className="flex gap-4 items-center">
+              <Icon
+                icon={"solar:upload-linear"}
+                className="text-5xl drop-shadow-md text-gray-600 group-hover:text-blue-500"
+              />
+            </div>
+            <input
+              type="file"
+              name=""
+              id=""
+              multiple
+              className="absolute inset-0 cursor-pointer opacity-0"
+            />
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
